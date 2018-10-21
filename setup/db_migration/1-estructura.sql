09b8035259085c41c638c0d62007a536fc4e004f
Gustavo Novaro <gnovaro@gmail.com>
2018-10-21 18:36:56 -0300

    Cambiamos la estrutura de la base
    Se añadieron FK faltantes a la DB.

--- setup/db_migration/1-estructura.sql
+++ setup/db_migration/1-estructura.sql
@@ -2,10 +2,12 @@
  * Preguntados ESTRUCTURA SQL
  * ESBA
  * GRUPO 1: Novaro, Sumi, Guido, Rodriguez
- * @version: 1.0.4
+ * @version: 1.0.5
  */
+-- Creamos la base de datos
 CREATE DATABASE preguntados;
 
+--Nos posicionamos en la base creada; de ahora en mas todos los scripts aplican a esta base
 USE preguntados;
 
 -- Creamos tabla idioma
@@ -17,7 +19,7 @@ CREATE TABLE idioma
 );
 
 -- Creamos LA PK sobre la columna id
-ALTER TABLE idioma ADD CONSTRAINT PK_idioma PRIMARY KEY (id); 
+ALTER TABLE idioma ADD CONSTRAINT PK_idioma PRIMARY KEY (id);
 
 --habilitamos permitir especificar id
 SET IDENTITY_INSERT idioma ON;
@@ -42,7 +44,7 @@ CREATE TABLE usuario
 );
 
 -- Creamos LA PK sobre la columna id
-ALTER TABLE usuario ADD CONSTRAINT PK_usuario PRIMARY KEY (id); 
+ALTER TABLE usuario ADD CONSTRAINT PK_usuario PRIMARY KEY (id);
 
 --Deshabilitamos el autoicremental temporalmente
 SET IDENTITY_INSERT usuario ON;
@@ -53,7 +55,7 @@ SET IDENTITY_INSERT usuario ON;
   * Concatenado al final del password y hasheado el resultado de la concatenacion en sha256
 */
 INSERT INTO usuario(id,nombre,email,idioma_id,contrasena) 
-VALUES(1,'Admin','grupo1@esba.com.ar',1,HASHBYTES('SHA2_256',CONCAT_WS('','4dm1n%','3$B4')));
+VALUES(1,'Admin','grupo1@esba.com.ar',1,HASHBYTES('SHA2_256','4dm1n%3$B4'));--Pass: 4m1n% Salt: 3$B4
 
 --habilitamos el autoicremental nuevamente
 SET IDENTITY_INSERT usuario OFF;
@@ -93,7 +95,7 @@ SET IDENTITY_INSERT categoria OFF;
 -- Creamos la tabla pregunta
 CREATE TABLE pregunta
 (
-	id bigint NOT NULL IDENTITY(1,1),
+	id bigint NOT NULL IDENTITY(1,1), 
 	idioma_id int NOT NULL,
 	categoria_id int NOT NULL,
 	descripcion varchar(100) NOT NULL,
@@ -101,7 +103,7 @@ CREATE TABLE pregunta
 	votos_positivos bigint NOT NULL default 0,
 	votos_negativos bigint NOT NULL default 0,
 	usuario_id bigint NOT NULL, --CREADOR pregunta
-	denunciada smallint NOT NULL DEFAULT 0,
+	denunciada smallint NOT NULL DEFAULT 0, -- 0 No denunciada, 1 - Denunciada
 	estado smallint NOT NULL DEFAULT 1
 );
 
@@ -126,7 +128,7 @@ CREATE TABLE respuesta
 	id bigint NOT NULL IDENTITY(1,1),
 	usuario_id bigint NOT NULL,
 	pregunta_id bigint  NOT NULL,
-	pregunta_opcion_id int  NOT NULL,
+	pregunta_opcion_id bigint  NOT NULL,
 	correctamente smallint
 );
 
@@ -135,13 +137,55 @@ ALTER TABLE respuesta ADD CONSTRAINT PK_respuesta PRIMARY KEY (id);
 																	   
 -- Creamos Foreign Key FK_pregunta_categoria
 ALTER TABLE pregunta ADD CONSTRAINT FK_pregunta_categoria FOREIGN KEY
-(categoria_id) REFERENCES dbo.categoria
-(id) ON UPDATE  NO ACTION 
+(categoria_id) REFERENCES categoria
+(id) ON UPDATE  CASCADE 
  ON DELETE  NO ACTION; 
 
 -- Creamos Foreign key FK_pregunta_idioma
 ALTER TABLE pregunta ADD CONSTRAINT FK_pregunta_idioma FOREIGN KEY
-(idioma_id) REFERENCES dbo.idioma
-(id) ON UPDATE  NO ACTION 
+(idioma_id) REFERENCES idioma
+(id) ON UPDATE  CASCADE 
 ON DELETE  NO ACTION;
 																	   
+-- Creamos Foreign Key FK_usuario_idioma
+ALTER TABLE usuario ADD CONSTRAINT
+FK_usuario_idioma FOREIGN KEY
+(idioma_id) REFERENCES idioma
+(id) ON UPDATE  CASCADE 
+ON DELETE  NO ACTION;
+
+-- Creamos FK_categoria_idioma
+ALTER TABLE categoria ADD CONSTRAINT
+FK_categoria_idioma FOREIGN KEY
+(idioma_id) REFERENCES idioma
+(id) ON UPDATE  CASCADE 
+	 ON DELETE  NO ACTION;
+
+-- Creamos FK_pregunta_opcion
+ALTER TABLE pregunta_opcion ADD CONSTRAINT
+FK_pregunta_opcion_pregunta FOREIGN KEY
+(pregunta_id) REFERENCES pregunta
+(id) ON UPDATE  CASCADE 
+ON DELETE  NO ACTION;
+
+
+-- Creamos FK respuesta_usuario
+ALTER TABLE respuesta ADD CONSTRAINT
+FK_respuesta_usuario FOREIGN KEY
+(usuario_id) REFERENCES usuario
+(id) ON UPDATE  CASCADE 
+	 ON DELETE  NO ACTION;
+
+-- Creamos FK respuesta_pregunta
+ALTER TABLE respuesta ADD CONSTRAINT
+FK_respuesta_pregunta FOREIGN KEY
+(pregunta_id) REFERENCES pregunta
+(id) ON UPDATE  CASCADE 
+	 ON DELETE  NO ACTION;
+
+-- Creamos FK respuesta_pregunta
+ALTER TABLE respuesta ADD CONSTRAINT
+FK_respuesta_pregunta_opcion FOREIGN KEY
+(pregunta_opcion_id) REFERENCES pregunta_opcion
+(id) ON UPDATE  CASCADE 
+	 ON DELETE  NO ACTION;
