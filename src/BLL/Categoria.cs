4390b3098d4bc0b84c14d83990f4adeb552b525b
dsumi10 <44279098+dsumi10@users.noreply.github.com>
2018-10-22 17:22:12 -0300

    Se agregan atributos a clase categoria

--- src/BLL/Categoria.cs
+++ src/BLL/Categoria.cs
@@ -6,7 +6,35 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    class Categoria
+    public class Categoria
     {
+        private int _id;
+        private Idioma _idioma;
+        private string _nombre;
+        private int _estado;
+        
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public Idioma idioma
+        {
+            get { return _idioma; }
+            set { _idioma = value; }
+        }
+
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        public int estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
     }
 }
