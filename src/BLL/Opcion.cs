2620e7e82c67fbed2ae471f364fbeb18babb44eb
dsumi10 <daniel-sumi@hotmail.com>
2018-10-22 23:20:15 -0300

    Se agregan atributos y clase
    Agrego clase opcion
    Agrego atributos en usuario, pregunta

---
+++ src/BLL/Opcion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    public class Opcion
+    {
+        private int _id;
+        private string _descripcion;
+        private int _correcta;
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        public int correcta
+        {
+            get { return _correcta; }
+            set { _correcta = value; }
+        }
+        
+        
+    }
+}
--- src/BLL/Pregunta.cs
+++ src/BLL/Pregunta.cs
@@ -6,7 +6,78 @@ using System.Threading.Tasks;
 
 namespace BLL
 {
-    class Pregunta
+    public class Pregunta
     {
+        private int _id;
+        private Idioma _idioma;
+        private Categoria _categoria;
+        private string _descripcion;
+        private int _votosPositivos;
+        private int _votosNegativos;
+        private Usuario _creador;
+        private int _denunciada;
+        private int _estado;
+        private List<Opcion> _opciones = new List<Opcion>();
+        public int id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public Idioma idioma
+        {
+            get { return _idioma; }
+            set { _idioma = value; }
+        }
+
+        public Categoria categoria
+        {
+            get { return _categoria; }
+            set { _categoria = value; }
+        }
+
+        public string descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+  
+        public int votosPositivos
+        {
+            get { return _votosPositivos; }
+            set { _votosPositivos = value; }
+        }
+
+        public int votosNegativos
+        {
+            get { return _votosNegativos; }
+            set { _votosNegativos = value; }
+        }
+
+        public Usuario creador
+        {
+            get { return _creador; }
+            set { _creador = value; }
+        }
+
+        public int denunciada
+        {
+            get { return _denunciada; }
+            set { _denunciada = value; }
+        }
+
+        public int estado
+        {
+            get { return _estado; }
+            set { _estado = value; }
+        }
+
+        public List<Opcion> opciones
+        {
+            get { return _opciones; }
+            set { _opciones = value; }
+        }
+        
+        
     }
 }
