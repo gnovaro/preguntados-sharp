d50bd2523a812ca5f5a3c4e3db383ce03c3c6feb
Gustavo Novaro <gnovaro@users.noreply.github.com>
2018-10-25 11:12:32 -0300

    Se añade otra pregunta de otra categoria

--- setup/db_migration/2-preguntas_espanol.sql
+++ setup/db_migration/2-preguntas_espanol.sql
@@ -16,4 +16,17 @@ VALUES(1,'Steven Spielberg',0);
 INSERT INTO pregunta_opcion(pregunta_id, descripcion_opcion, correcta) 
 VALUES(1,'Ridley Scott',0);
 
+-- Preguntas categoria 4 Video Juegos
+INSERT INTO pregunta(id,idioma_id,categoria_id,descripcion, opcion_id_correcta,usuario_id) 
+VALUES(2,1,4,'¿En qué juego apareció Mario por primera vez?',6,1);
+
+INSERT INTO pregunta_opcion(pregunta_id, descripcion_opcion, correcta) 
+VALUES(2,'The Legend of Zelda',0);
+INSERT INTO pregunta_opcion(pregunta_id, descripcion_opcion, correcta) 
+VALUES(2,'Donkey Kong',1);
+INSERT INTO pregunta_opcion(pregunta_id, descripcion_opcion, correcta) 
+VALUES(2,'Dig-dug',0);
+INSERT INTO pregunta_opcion(pregunta_id, descripcion_opcion, correcta) 
+VALUES(2,'Super Mario Brothers',0);
+
 SET IDENTITY_INSERT pregunta OFF;
