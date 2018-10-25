1393155d2e971860c43f200b325a3f3bac7c540d
dsumi10 <daniel-sumi@hotmail.com>
2018-10-24 23:29:29 -0300

    Se agrega dos formas de hacer alta de opcionDal

--- src/BLL/Opcion.cs
+++ src/BLL/Opcion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DAL;
 
 namespace BLL
 {
@@ -28,7 +29,27 @@ namespace BLL
             get { return _correcta; }
             set { _correcta = value; }
         }
-        
-        
+
+        public void alta(int preguntaId) 
+        {
+            OpcionDAL objopciondal = new OpcionDAL();
+
+            objopciondal.alta(preguntaId,this._descripcion,this._correcta);
+        }
+
+        public void altaCorrecta(int preguntaId)
+        {
+            OpcionDAL objopciondal = new OpcionDAL();
+            int valor;
+
+            valor = objopciondal.altaCorrecta(preguntaId, this._descripcion , this._correcta);
+
+            //En valor obtengo el id luego de hacer el insert y se lo paso al objeto
+            if (valor != -1)
+            {
+                this._id = valor;
+            }
+            //si es -1 es porque fallo el insert, quiza deberia retornar algo este alta
+        }
     }
 }
--- src/DAL/OpcionDAL.cs
+++ src/DAL/OpcionDAL.cs
@@ -4,15 +4,53 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using System.Data.SqlClient;
 
 namespace DAL
 {
-    class OpcionDAL
+    public class OpcionDAL
     {
         /*public DataTable obtenerOpcionesPorPregunta(int PreguntaId)
         {
 
         }*/
+
+        public int alta(int preguntaId,string descripcion,int correcta) 
+        {
+            Conexion objconexion = new Conexion();
+            string procedimiento = "opcion_alta";
+            int filasafectadas;
+
+            SqlParameter[] parametros = new SqlParameter[3];
+
+            parametros[0] = objconexion.crearParametro("@p_pregunta_id", preguntaId);
+            parametros[1] = objconexion.crearParametro("@p_descripcion_opcion", descripcion);
+            parametros[2] = objconexion.crearParametro("@p_correcta", correcta);
+
+            filasafectadas = objconexion.EscribirPorStoreProcedure(procedimiento, parametros);
+
+            return filasafectadas;
+
+        }
+
+        public int altaCorrecta(int preguntaId, string descripcion, int correcta) 
+        {
+            Conexion objconexion = new Conexion();
+            string procedimiento = "opcion_alta";
+            int filavalor;
+            string variable = "@p_id";
+
+            SqlParameter[] parametros = new SqlParameter[4];
+
+            parametros[0] = objconexion.crearParametroDeSalida("@p_id");
+            parametros[1] = objconexion.crearParametro("@p_pregunta_id", preguntaId);
+            parametros[2] = objconexion.crearParametro("@p_descripcion_opcion", descripcion);
+            parametros[3] = objconexion.crearParametro("@p_correcta", correcta);
+
+            filavalor = objconexion.EscribiryObtenerValorSP(procedimiento, parametros, variable);
+            //Aca devuelvo el valor del id despues de hacer el insert y si fallo devuelve -1
+            return filavalor;
+        }
         
     }
 }
